16c215f2fa4d836f0e34b9494e96199d3a9ab07e
gajdosech2 <l.gajdosech@gmail.com>
2020-02-20 19:17:51 +0100

    read file

--- main.cpp
+++ main.cpp
@@ -1,25 +1,79 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <map>
 
 const std::string dataset_path = "../../../input/";
 const std::string output_path = "../../../output/";
 
-int main(int argc, char *argv[])
+int books;
+int num_libraries;
+int days;
+std::vector<std::pair<int, int>> scores;
+
+struct Library
 {
-  std::string line;
-  std::ifstream text_file;
-  text_file.open(dataset_path + "/test_file.txt");
-  if (!text_file.is_open()) {
-    return 1;
-  }
-  while (!text_file.eof())
+  int number_books;
+  int signup_time;
+  int books_day;
+  std::vector<int> books;
+
+  int Score()
   {
-    text_file >> line;
-    std::cout << line;
+    int score = 0;
+    for (int id : books)
+    {
+      score += scores[id].second;
+    }
+    return score;
+  }
+};
+
+std::vector<Library> libs;
+
+int ReadFile(std::string file)
+{
+  int number;
+  std::ifstream text_file;
+  text_file.open(dataset_path + "/" + file + ".txt");
+  if (!text_file.is_open())
+  {
+    return EXIT_FAILURE;
+  }
+  text_file >> books;
+  text_file >> num_libraries;
+  text_file >> days;
+
+  for (int i = 0; i < books; i++)
+  {
+    int score_for_book;
+    text_file >> score_for_book;
+    scores.push_back(std::pair<int, int>(i, score_for_book));
+  }
+
+  for (int i = 0; i < num_libraries; i++)
+  {
+    Library l;
+    text_file >> l.number_books;
+    text_file >> l.signup_time;
+    text_file >> l.books_day;
+    for (int j = 0; j < l.number_books; j++)
+    {
+      int book_id;
+      text_file >> book_id;
+      l.books.push_back(book_id);
+    }
+    libs.push_back(l);
   }
   text_file.close();
+}
 
+int main(int argc, char *argv[])
+{
   std::cout << "Hello world!";
-
+  ReadFile("a_example");
   return 0;
 }
+
+
+
